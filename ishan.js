3b5e7efc4155874ef6d910f4c3fbbd73c244a80e
MehediHasan25 <ishanmehadi@gmail.com>
2025-05-09 22:16:29 +0600

    commit main

--- ishan.js
+++ ishan.js
@@ -1,6 +1,9 @@
 
-let a = 'Almost finished the training';
+let a = 'Git Training ';
 let b = 20; 
 let c =30;
 let totalCounting = b+c;
 console.log(a,totalCounting);
+function save(a){
+    console.log(`your desired variable is ${a}`);
+}
