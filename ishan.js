471a9776ce8a2d91e8fcf4a87663df68a102da74
MehediHasan25 <ishanmehadi@gmail.com>
2025-05-09 17:57:10 +0600

    DONE

--- ishan.js
+++ ishan.js
@@ -1,6 +1,6 @@
 
-let a = 'Git Training for MIS Team';
-let b = 20; 
+let a = 'Git Training for MIS Team with new joining ';
+let b = 2000; 
 let c =30;
-let total = b+c;
-console.log(a,total);
+let totalCounting = b+c;
+console.log(a,totalCounting);
