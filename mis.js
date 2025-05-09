54d9c919da166685f6a31916196c46cde2ba32d6
MehediHasan25 <ishanmehadi@gmail.com>
2025-05-09 17:48:33 +0600

    Check

--- mis.js
+++ mis.js
@@ -1,9 +1,8 @@
+function data(a,b,c){
+    return a,b,c;
+}
+
 let alpha =['Executive,sr.Executive,Manager,Assistant Manager'];
-let additiosn = (a,b)=> a+b;
-let attendance = addition(5,4);
-console.log(attendance);
 
-
-console.log (designation);
 
 
