2e7bf97016fef046c5fc6d3f2226e5cdd6544bb8
MehediHasan25 <ishanmehadi@gmail.com>
2025-05-09 17:34:37 +0600

    This is my branch commit

--- mis.js
+++ mis.js
@@ -1,2 +1,9 @@
-let team =['Executive,sr.Executive,Manager,Assistant Manager'];
-console.log (team);
+let designation =['Executive,sr.Executive,Manager,Assistant Manager'];
+let addition = (a,b)=> a+b;
+let attendance = addition(5,4);
+console.log(attendance);
+
+
+console.log (designation);
+
+
