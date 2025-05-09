88a2fdf2ac1a87cd446c6656f5443de21e018f60
MehediHasan25 <ishanmehadi@gmail.com>
2025-05-09 18:06:41 +0600

    Checking Branch

--- ishan.js
+++ ishan.js
@@ -1,11 +1,6 @@
 
-<<<<<<< HEAD
-let a = 'Git Training for MIS Team with new joining ';
-let b = 2000; 
-=======
 let a = 'Git Training ';
 let b = 20; 
->>>>>>> main
 let c =30;
 let totalCounting = b+c;
 console.log(a,totalCounting);
