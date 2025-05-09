c039f8c87da30cb6288d7b67b4b80bd4929025f9
MehediHasan25 <ishanmehadi@gmail.com>
2025-05-09 17:43:26 +0600

    MY main data

--- mis.js
+++ mis.js
@@ -1,13 +1,9 @@
-let designation =['Executive,sr.Executive,Manager,Assistant Manager','Deputy Manger'];
-let substraction = (a,b)=> a-b;
-let multiple =(c,d)=> c*d;
-let count = substraction(5,4);
-let count2 = multiple(24,2);
+let alpha =['Executive,sr.Executive,Manager,Assistant Manager'];
+let additiosn = (a,b)=> a+b;
+let attendance = addition(5,4);
+console.log(attendance);
 
-console.log(count,count2);
 
-for(let i =0; i< designation.length; i++){
-    console.log(designation[i]);
-}
+console.log (designation);
 
 
